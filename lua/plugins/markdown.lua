513fa4b017c1a64c580f244372a766b7fdc65105
lonicera <lonicera.xyz@gmail.com>
2024-10-24 14:48:37 +0800

    Init nvim configure project

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,54 @@
+return {
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = function()
+			require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+			vim.fn["mkdp#util#install"]()
+		end,
+		keys = {
+			{
+				"<leader>cp",
+				ft = "markdown",
+				"<cmd>MarkdownPreviewToggle<cr>",
+				desc = "Markdown Preview",
+			},
+		},
+		config = function()
+			vim.cmd([[do FileType]])
+		end,
+	},
+
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {
+			code = {
+				sign = false,
+				width = "block",
+				right_pad = 1,
+			},
+			heading = {
+				sign = false,
+				icons = {},
+			},
+		},
+		ft = { "markdown", "norg", "rmd", "org" },
+		config = function(_, opts)
+			require("render-markdown").setup(opts)
+			LazyVim.toggle.map("<leader>um", {
+				name = "Render Markdown",
+				get = function()
+					return require("render-markdown.state").enabled
+				end,
+				set = function(enabled)
+					local m = require("render-markdown")
+					if enabled then
+						m.enable()
+					else
+						m.disable()
+					end
+				end,
+			})
+		end,
+	},
+}
